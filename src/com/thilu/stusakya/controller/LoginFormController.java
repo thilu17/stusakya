f7d61794b4b28fd9b1ea6436a60bbdd7131f1091
Thilu Weliwaththa <thilakshiis@gmail.com>
2023-05-17 21:00:34 +0530

    Login button created

--- src/com/thilu/stusakya/controller/LoginFormController.java
+++ src/com/thilu/stusakya/controller/LoginFormController.java
@@ -1,9 +1,12 @@
 package com.thilu.stusakya.controller;
 
 import com.jfoenix.controls.JFXTextField;
+import com.thilu.stusakya.db.Database;
+import com.thilu.stusakya.model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -17,17 +20,33 @@ public class LoginFormController {
 
 
     public void btnLoginOnAction(ActionEvent actionEvent) {
+        String email = txtEmail.getText().toLowerCase().trim();
+        String password = txtPassword.getText().trim();
+
+        for (User user : Database.userTable) {
+            if (user.getEmail().equals(email)) {
+                if (user.getPassword().equals(password)) {
+                    System.out.println(user.toString());
+                    return;
+                } else {
+                    new Alert(Alert.AlertType.ERROR, "Wrong Password!").show();
+                    return;
+                }
+            }
+        }
+        new Alert(Alert.AlertType.WARNING,
+                String.format("User not found (%s)", email)).show();
     }
 
-    public void btnCreateAnAccountOnAction(ActionEvent actionEvent) {
+    public void btnCreateAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("SignUpForm");
     }
-
 
 
     private void setUi(String location) throws IOException {
         Stage stage = (Stage) context.getScene().getWindow();
         stage.setScene(new Scene(
-                FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+                FXMLLoader.load(getClass().getResource("../view/" + location + ".fxml"))));
         stage.centerOnScreen();
     }
 }
