d23bd4e12480e0c635ba4e41819e6f6939375569
Thilu Weliwaththa <thilakshiis@gmail.com>
2023-05-17 17:08:10 +0530

    Initial commit

---
+++ src/com/thilu/stusakya/controller/LoginFormController.java
@@ -0,0 +1,21 @@
+package com.thilu.stusakya.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+    public AnchorPane context;
+    public JFXTextField txtEmail;
+    public JFXTextField txtPassword;
+
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCreateAnAccountOnAction(ActionEvent actionEvent) {
+    }
+
+
+}
---
+++ src/com/thilu/stusakya/controller/SignUpFormController.java
@@ -0,0 +1,19 @@
+package com.thilu.stusakya.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class SignUpFormController {
+    public AnchorPane context;
+    public JFXTextField txtFirstName;
+    public JFXTextField txtEmail;
+    public JFXTextField txtLastName;
+    public JFXTextField txtPassword;
+
+    public void btnSignUpOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAlreadyHaveAnAccountOnAction(ActionEvent actionEvent) {
+    }
+}
